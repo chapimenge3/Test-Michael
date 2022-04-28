5650cbfc55a7e8bd19bd2210af3d8e922a7655c0
Chapi Menge <chapimenge3@gmail.com>
2022-04-28 15:48:57 +0300

    add: implement test for the api

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # Test-Michael
 This is test project intended for Michael.T.
+
+# How to run the project
+
+    1. doker compose up( if this is not working follow the below steps)
+    2. docker build
+    3. docker compose up( to detach and run it in background add -d to the command `docker compose up -d`)
+    4. docker-compose up( if you are not using docker the latest version of docker)
+
+# How to Test
+
+Tests are implemented to check the validity of the api endpoint and the models. To run the test we need to run the following command:
+
+    1. docker compose run web python testproject/manage.py test api
+
+# Available Command 
+
+    - docker compose run web python testproject/manage.py savecountries - save countries to database
+    - docker compose run web python testproject/manage.py saveloans - save loans to database
+    - docker compose run web python testproject/manage.py saveprojects - save projects to database
+    - docker compose run web python testproject/manage.py savesectors - save sectors to database
+
+# Avaialable Endpoints
+
+    - http://localhost:8000/api/countries/ - get all countries
+    - http://localhost:8000/api/loans/ - get loans 
+    - http://localhost:8000/api/projects/ - get projects
+    - http://localhost:8000/api/sectors/ - get sectors
+    - http://localhost:8000/api/filters/ - get filters
+
+# Available Filters
+
+for all of the api except for the filters endpoint, you can use the following filters:
+
+    - sortColumn
+    - sortDir
+    - pageNumber
+    - itemPerPage
+    - pageable
+    - language
+    - defaultLanguage
+    - loanPartYearFrom
+    - loanPartYearTo
+    - orCountries
+    - orCountries
+    - orSectors
+
+Just like the https://www.eib.org website, the filters are applied in the endpoints too.
+
+
+Developer: Temkin Mengistu.
+Email: chapimenge3@gmail.com
--- testproject/api/tests.py
+++ testproject/api/tests.py
@@ -1,3 +1,83 @@
+from rest_framework import test
+
+from django.urls import reverse
 from django.test import TestCase
 
-# Create your tests here.
+from api.models import Projects, Country, Sector, Loans
+
+
+class APITestCase(test.APITestCase):
+
+    def test_project(self):
+        url = reverse("testapi-projects")
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue(isinstance(response.data, list))
+
+    def test_countries(self):
+        url = reverse("testapi-countries")
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue(isinstance(response.data, list))
+        self.assertTrue(isinstance(response.data[0], dict))
+
+    def test_sectors(self):
+        url = reverse("testapi-sectors")
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue(isinstance(response.data, list))
+        self.assertTrue(isinstance(response.data[0], dict))
+
+    def test_loans(self):
+        url = reverse("testapi-loans")
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue(isinstance(response.data, list))
+        self.assertTrue(isinstance(response.data[0], dict))
+
+
+class ModelTestCase(TestCase):
+
+    def test_sector(self):
+        sector = Sector.objects.create(value="value", label="label")
+        self.assertTrue(isinstance(sector, Sector))
+        self.assertEqual(sector.value, "value")
+        self.assertEqual(sector.label, "label")
+
+    def test_country(self):
+        country = Country.objects.create(value="value", label="label")
+        self.assertTrue(isinstance(country, Country))
+        self.assertEqual(country.value, "value")
+        self.assertEqual(country.label, "label")
+
+    def test_project(self):
+        sector = Sector.objects.create(value="value", label="label")
+        country = Country.objects.create(value="value", label="label")
+        project = Projects.objects.create(
+            sector=sector,
+            country=country,
+            title="title")
+
+        self.assertTrue(isinstance(project, Projects))
+        self.assertEqual(project.title, "title")
+        self.assertEqual(project.sector, sector)
+        self.assertEqual(project.country, country)
+    
+    def test_loan(self):
+        sector = Sector.objects.create(value="value", label="label")
+        country = Country.objects.create(value="value", label="label")
+        project = Projects.objects.create(
+            sector=sector,
+            country=country,
+            title="title")
+        loans = Loans.objects.create(
+            project=project,
+            amount="$100",
+            date="1 April 2020",
+        )
+        
+        self.assertTrue(isinstance(loans, Loans))
+        self.assertEqual(loans.project, project)
+        self.assertEqual(loans.amount, "$100")
+        self.assertEqual(loans.date, "1 April 2020")
+
